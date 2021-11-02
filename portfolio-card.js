508e9661e836decdc5720d6145249b8f93158e31
LutsenkoDm <lutsenkodm@mail.ru>
2021-11-02 13:43:28 +0300

    Change filters to color vars

--- portfolio-card.js
+++ portfolio-card.js
@@ -1,7 +1,9 @@
-function changeTheme() {
-    let portfolioCardClassList = document.querySelector("#portfolio-card").classList;
-    portfolioCardClassList.toggle("dark-theme");
-    portfolioCardClassList.toggle("light-theme");
-}
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.querySelector("#portfolio-card-theme-switcher").addEventListener('click', changeTheme);
+});
 
-document.querySelector("#portfolio-card-theme-switcher").addEventListener('click', changeTheme);
+function changeTheme() {
+    let bodyCardClassList = document.querySelector("body").classList;
+    bodyCardClassList.toggle("dark-theme");
+    bodyCardClassList.toggle("light-theme");
+}
