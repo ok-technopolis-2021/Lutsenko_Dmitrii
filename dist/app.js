b7702897bb87bb291e138eb7965294fdb2788b25
LutsenkoDm <lutsenkodm@mail.ru>
2021-11-02 13:24:43 +0300

    Fix comments

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.querySelector("#portfolio-card-theme-switcher").addEventListener('click', changeTheme);
+});
+
 function changeTheme() {
     let bodyCardClassList = document.querySelector("body").classList;
     bodyCardClassList.toggle("dark-theme");
     bodyCardClassList.toggle("light-theme");
 }
-
-document.addEventListener("DOMContentLoaded",  changeTheme);
-document.querySelector("#portfolio-card-theme-switcher").addEventListener('click', changeTheme);
